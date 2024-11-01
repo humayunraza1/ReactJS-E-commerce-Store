223cb0484c197c734971a0473e5a9bc856ce9492
Humayun Raza <humayunraza5@gmail.com>
2024-11-02 04:17:14 +0500

    making cookie work with cors :(: ~H

--- server/app.js
+++ server/app.js
@@ -13,16 +13,10 @@ const allowedOrigins = ['https://react-js-e-commerce-store-client.vercel.app'];
 app.use(
   cors({
     origin: allowedOrigins,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
   })
 );
-
-// Handle preflight requests for all routes
-app.options('*', cors(corsOptions), (req, res) => {
-  res.sendStatus(200); // Respond with HTTP 200 OK status for OPTIONS requests
-});
 
 app.use('/api/auth', authRoutes);
 app.use('/users',  userRoutes);
