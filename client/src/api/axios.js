a59766acfd02e0e9aa200e3cab25ecbebfe6bdd2
Humayun Raza <humayunraza5@gmail.com>
2024-11-11 01:39:05 +0500

    mobile sign in component ~H

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 const BASE_URL = 'https://api.azzyshardware.store'
+const LOCAL_URL = 'http://localhost:3000'
 
 export default axios.create({
-    baseURL: BASE_URL,
+    baseURL:BASE_URL,
     withCredentials:true
 })
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL:BASE_URL,
     headers:{'Content-Type': 'application/json'},
     withCredentials: true
 })
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -130,7 +130,7 @@ function Login () {
     return (
         <>
         <div className={`flex justify-center items-center w-screen h-screen ${darkMode ? 'bg-[#0a1018]':'bg-white'}`}>
-            <div className={`${darkMode && "bg-black"} card shadow-2xl w-[800px] h-96 rounded-2xl border-black flex`}>
+            <div className={`${darkMode && "bg-black"} hidden card shadow-2xl w-[800px] h-96 rounded-2xl border-black md:flex`}>
                 <div className="w-[50%] p-8 rounded-l-2xl flex justify-center">
                 <div className={`w-[250px]`}>
 
@@ -197,8 +197,43 @@ function Login () {
                 </div>
                 </div>
             </div>
+
+        <>
+        <div className=" md:hidden flex flex-col w-[350px] rounded-xl h-[400px] shadow-2xl">
+        <div className="w-full p-2 h-16 bg-slate-50 rounded-t-2xl flex justify-center items-center text-center">
+          <h1 className="font-Bebas text-xl">Azzy's Harware</h1>
         </div>
-        {/* <ClerkSign/> */}
+        <div className="h-[280px] w-full flex flex-col justify-center items-center z-10 rounded-t-2xl">
+        <div>
+      <Label className={`mt-5 ${darkMode ? "text-white":"text-black"}`} htmlFor="email" >Email</Label>
+      <Input disabled={isLoading} onChange={handleInputChange} type="email" name="email" id="email" placeholder="Email" className="w-[250px]"/>
+                    </div>
+        <div className="mt-2">
+      <Label className={`mt-5 ${darkMode ? "text-white":"text-black"}`} htmlFor="password">Password</Label>
+      <Input disabled={isLoading} onChange={handleInputChange} type="password" name="password" id="password" placeholder="Password"  className="w-[250px]"/>
+        </div>
+        <div className="flex w-[71%] items-start">
+
+        <Button disabled={isLoading} onClick={login} className="mt-3">
+            { isLoading ? <> <Loader2 className="mr-2 h-4 w-4 animate-spin"/> Logging in </>:<>
+                Login
+            </>
+                
+              }
+            
+            </Button>
+              </div>
+        </div>
+        <div className="bg-slate-900 p-5 text-center text-white rounded-b-xl z-0 w-full">
+                                           Haven't signed up?
+                        
+                    <Button className="text-red ml-5" variant='ghost' onClick={()=>navigate('/register')}>Register</Button>
+                </div>
+
+        </div>
+        </>
+        </div>
+
         </>
     );
 };
